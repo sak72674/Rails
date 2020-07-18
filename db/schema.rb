9933c6f0496ec7e8511666627f333ed3bf2ab671
小田切彩織 <saori@saoris-MacBook-Air.local>
2020-07-18 19:19:03 +0900

    Finish 3-5

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_17_111225) do
+ActiveRecord::Schema.define(version: 2020_07_18_084645) do
 
   create_table "lists", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "body"
   end
 
 end
